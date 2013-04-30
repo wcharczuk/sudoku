868d25ece378f5d6b59688a345ef4095f673b9de
Will Charczuk <will@clotheshor.se>
2013-04-30 10:50:08 -0400

    adding some sample puzzles.

---
+++ Sudoku.Console/Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using Sudoku.Core;
+using System.Collections.Generic;
+
+namespace Sudoku.Console
+{
+	class Program
+	{
+		public static void Main (string[] args)
+		{
+			Stream inputStream = null;
+		
+			if(args.Length > 0)
+				inputStream = File.Open(args[0], FileMode.Open);
+			else
+				inputStream = System.Console.OpenStandardInput();
+			
+			var board = new Board();
+			using(StreamReader sr = new StreamReader(inputStream))
+			{
+				int row = 0;
+				while(!sr.EndOfStream)
+				{
+					var line = sr.ReadLine();
+					
+					String[] pieces = null;
+					
+					if(line.Contains(","))
+						pieces = line.Split(',');
+					else if(line.Contains(" "))
+						pieces = line.Split(' ');
+					
+					int col = 0;
+					foreach(string val in pieces)
+					{
+						if(val == "-" || val == "0" || String.IsNullOrEmpty(val))
+							board[row,col] = null;
+						else
+							board[row, col] = short.Parse(val);
+						
+						col++;
+					}
+					
+					row++;
+				}
+			}
+			
+			Solver solver = new Solver(board);
+			var solution = solver.Solve();
+			
+			for(int row = 0; row < 9; row++)
+			{
+				var values = new List<short?>();
+				for(int col = 0; col < 9; col++)
+				{
+					values.Add (solution[row, col]);
+				}
+				
+				System.Console.WriteLine(String.Join(",", values));
+			}
+
+		}
+	}
+}
